d5e097d5eeb130fe1cfc9cb81cb390bb8f3efe2d
Amber <fiddlemouth@gmail.com>
2022-10-19 19:25:46 +0200

    Puppet mostly done except axe

--- PotionPuppet.gd
+++ PotionPuppet.gd
@@ -3,15 +3,54 @@ extends Node2D
 var target = "left"
 var rotation_speed = 0.0
 
+var timer = 0.0
+var foot_target_offset = 60
+
+var axe_side = 1 # {-1, 1} corresponds to left & right
+
 func _process(delta):
+	timer += delta
+	
+	## DEBUG: walk left n right with arrows
+	if Input.is_action_pressed("ui_left"):
+		position.x -= 300 * delta
+		$FootLeft.default_foot_offset = (Vector2.LEFT * foot_target_offset) + Vector2(rand_range(-5,5), rand_range(-5,5) + 80)
+		$FootRight.default_foot_offset = (Vector2.LEFT * foot_target_offset) + Vector2(rand_range(-5,5), rand_range(-5,5) + 80)
+	if Input.is_action_pressed("ui_right"):
+		position.x += 300 * delta
+		$FootLeft.default_foot_offset = (Vector2.RIGHT * foot_target_offset) + Vector2(rand_range(-5,5), rand_range(-5,5) + 80)
+		$FootRight.default_foot_offset = (Vector2.RIGHT * foot_target_offset) + Vector2(rand_range(-5,5), rand_range(-5,5) + 80)
+	if Input.is_action_pressed("ui_up"):
+		position.y -= 300 * delta
+		$FootLeft.default_foot_offset = (Vector2.UP * foot_target_offset) + Vector2(rand_range(-5,5), rand_range(-5,5) + 80)
+		$FootRight.default_foot_offset = (Vector2.UP * foot_target_offset) + Vector2(rand_range(-5,5), rand_range(-5,5) + 80)
+	if Input.is_action_pressed("ui_down"):
+		position.y += 300 * delta
+		$FootLeft.default_foot_offset = (Vector2.DOWN * foot_target_offset) + Vector2(rand_range(-5,5), rand_range(-5,5) + 80)
+		$FootRight.default_foot_offset = (Vector2.DOWN * foot_target_offset) + Vector2(rand_range(-5,5), rand_range(-5,5) + 80)
+	
+	## DEBUG: Swing Axe with mouse
+	if Input.is_action_just_pressed("player_click"):
+		axe_side *= -1
 	
 	look_at_mouse()
 	animate_stem(delta)
+	animate_axe(delta)
+	
+	# Bob body
+	$Body.position.y = 5*sin(timer*4)
+	$BodyShadow.scale.x = .8 + .1*sin(timer*4)
+	$BodyShadow.scale.y = .5 + .1*sin(timer*4)
 
 
 func look_at_mouse():
-	$Body/Face.position = ( (get_global_mouse_position()-global_position)/300.0 )*20;
-	$Body/Face/Pupils.position = ( (get_global_mouse_position()-global_position)/300.0 )*20;
+	$Body/Face.position = ( (get_global_mouse_position()-$Body.global_position).normalized() )*20;
+	$Body/Face/Pupils.position = ( (get_global_mouse_position()-$Body.global_position)/300.0 )*20;
+	
+#	if $Body/Face.position.y < 0:
+#		$Body/Face.z_index = -1
+#	else:
+#		$Body/Face.z_index = 1
 
 
 func animate_stem(delta):
@@ -27,7 +66,14 @@ func animate_stem(delta):
 		rotation_speed = lerp(rotation_speed, (90 - $Body/stem.rotation_degrees)*30, delta*20)
 		$Body/stem.rotation_degrees += rotation_speed * delta;
 
-
-func feet():
-	pass
-
+var axe_position_speed = 0.0
+var axe_rotation_speed = 0.0
+func animate_axe(delta):
+#	var axe_target_position = ((get_global_mouse_position()-$Body.global_position).normalized().rotated(-PI/3*axe_side) )*100
+#	axe_position_speed = lerp(axe_position_speed, (axe_target_position - $Axe.position)*30, delta*20)
+#	$Axe.position += axe_position_speed * delta 
+	
+	var target_rotation = (get_global_mouse_position()-$Axe.global_position).angle() - PI/2 * axe_side
+	axe_rotation_speed = lerp(axe_rotation_speed, (target_rotation-$Axe.rotation)*30, delta*20)
+	$Axe.rotation += axe_rotation_speed * delta
+	
--- scenes/Foot.gd
+++ scenes/Foot.gd
@@ -1,31 +1,80 @@
-extends Node2D
+extends AnimatedSprite
 
-export (Vector2) var target_offset
-var target
-var moving = true
-onready var last_parent_position = get_parent().global_position
-var rotation_speed = 0
+const SPEED = 11
+
+export (Vector2) var default_foot_offset
+export (float) var distance_threshold
+export (Array, NodePath) var sibling_paths
+
+var siblings = []
+var is_animating := false
+
+onready var last_parent_position:Vector2 = get_parent().global_position
+onready var parent:Node2D = get_parent()
+
+func _ready():
+	# Add references to siblings
+	for path in sibling_paths:
+		siblings.append(get_node(path))
+
 
 func _process(delta):
-	# Uninherit parent position.
-	var parent_position_delta = last_parent_position - get_parent().global_position
+	# Uninherit parent position:
+	var parent_position_delta = last_parent_position - parent.global_position
 	global_position += parent_position_delta
-	last_parent_position = get_parent().global_position
-	
-	# Take step if too far from target.
-	target = get_parent().global_position + target_offset
-	
-	if !moving and global_position.distance_to(target) > 100:
-		moving = true
-	
-	var displacement = (target - global_position)
-	if moving:
-		global_position += 160 * displacement/20.0 * delta
+	last_parent_position = parent.global_position
+
+	# Check if foot should start animating:
+	if !is_animating \
+	and global_position.distance_to(last_parent_position+default_foot_offset) > distance_threshold:
+		# Check if siblings are already animating
+		var can_animate = true
+		for sibling in siblings:
+			if sibling.is_animating:
+				can_animate = false
 		
-		if global_position.distance_to(target) < 5:
-			moving = false
+		# If no other sibling is animating, then animate.
+		if can_animate:
+			frame = 1
+			is_animating = true
+			
 	
-	# Face direction of movement
-	var direction = displacement.angle()
-	rotation_speed = lerp(rotation_speed, (direction - rotation)*30, delta*20)
-	rotation += rotation_speed * delta;
+	# Process animation (if applicable):
+	if is_animating:
+		# Moov:
+		global_position = lerp(global_position, last_parent_position + default_foot_offset, delta * SPEED)
+
+		# End animation if reached default position:
+		if global_position.distance_to(last_parent_position+default_foot_offset) < 20:
+			frame = 0
+			is_animating = false
+
+#export (Vector2) var target_offset
+#var target
+#var moving = true
+#onready var last_parent_position = get_parent().global_position
+#var rotation_speed = 0
+#
+#func _process(delta):
+#	# Uninherit parent position.
+#	var parent_position_delta = last_parent_position - get_parent().global_position
+#	global_position += parent_position_delta
+#	last_parent_position = get_parent().global_position
+#
+#	# Take step if too far from target.
+#	target = get_parent().global_position + target_offset
+#
+#	if !moving and global_position.distance_to(target) > 100:
+#		moving = true
+#
+#	var displacement = (target - global_position)
+#	if moving:
+#		global_position += 160 * displacement/20.0 * delta
+#
+#		if global_position.distance_to(target) < 5:
+#			moving = false
+#
+#	# Face direction of movement
+#	var direction = displacement.angle()
+#	rotation_speed = lerp(rotation_speed, (direction - rotation)*30, delta*20)
+#	rotation += rotation_speed * delta;
