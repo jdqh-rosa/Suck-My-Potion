121ff034acd4ea1a408c0c6653d32e9c0a3e571f
jdqh <judah.rosaria@gmail.com>
2022-10-19 12:02:37 +0200

    Create Sword.cs

---
+++ Sword.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public class Sword : KinematicBody2D
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+
+	}
+
+	public void GetInput(){
+		LookAt(GetGlobalMousePosition());
+		
+		if(Input.IsActionPressed("player_click")){
+			Attack();
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		GetInput();
+	}
+
+	private void Attack(){
+
+	}
+}
