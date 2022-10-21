280c3ddfb2c0572e33df2786b03bbd198464f856
jdqh <judah.rosaria@gmail.com>
2022-10-21 11:09:31 +0200

    Player takes damage on contact with Enemy

--- Enemy.cs
+++ Enemy.cs
@@ -10,11 +10,22 @@ public class Enemy : KinematicBody2D
 	public Vector2 velocity = new Vector2();
 	private Vector2 playerPos = new Vector2();
 
+	private SignalManager signalManager;
+
+	[Signal] 
+	public delegate void Hit(float damage);
+
 	KinematicBody2D player;
 	
 	public override void _Ready(){
 		player = GetParent().GetNode<KinematicBody2D>("Player");
 		SetPlayerDirection();
+		signalManager = GetNode<SignalManager>("/root/SignalManager");
+		signalManager.Connect("player_moved", this, "on_player_moved");
+	}
+
+	void on_check(bool check){
+		GD.Print("check check 123:" + check);
 	}
 	public override void _PhysicsProcess(float delta)
 	{
@@ -25,7 +36,25 @@ public class Enemy : KinematicBody2D
 	}
 	
 	public void SetPlayerDirection(){
-		var direction = (player.Position - Position).Normalized();
+		//var direction = (player.Position - Position).Normalized();
+		var direction = (playerPos - Position).Normalized();
 		velocity = direction;
 	}
+
+	void _on_Area2D_body_entered(Area2D body)
+	{
+		if(body.Name == "Player"){
+			GD.Print("Collision with Player");
+			signalManager.EmitSignal("player_hit", damage);
+		}
+	}
+
+	private void on_player_moved(float newx, float newy){
+		playerPos = new Vector2(newx, newy);
+	}
+
+	private void on_player_hit(float damage){
+		signalManager.EmitSignal("player_hit", damage);
+	}
+
 }
--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -3,11 +3,28 @@ using System;
 
 public class PlayerMovement : KinematicBody2D
 {
+	[Export] public float health = 5;
    	[Export] public int speed = 200;
-
-	public Vector2 velocity = new Vector2();
+	
 	
 
+	public Vector2 velocity = new Vector2();
+	SignalManager signalManager;
+	
+	public override void _Ready()
+	{
+		signalManager = GetNode<SignalManager>("/root/SignalManager");
+		signalManager.Connect("player_hit", this, "_on_take_damage");
+
+		//GetTree().Connect("node_added", this, "_on_node_added");
+	}
+
+	public override void _Process(float delta)
+  	{
+		if(health<=0){
+			this.Visible = false;
+		}
+  	}
 	public void GetInput()
 	{	
 		LookAt(GetGlobalMousePosition());
@@ -33,5 +50,26 @@ public class PlayerMovement : KinematicBody2D
 	{
 		GetInput();
 		velocity = MoveAndSlide(velocity);
+		for (int i = 0; i < GetSlideCount(); i++)
+		{
+			var collision = GetSlideCollision(i);
+			//GD.Print("I collided with ", ((Node)collision.Collider).Name);
+		}
+		signalManager.EmitSignal("player_moved", Position.x, Position.y);
+	}
+	private void _on_PlayerBody_body_entered(KinematicBody2D body)
+	{
+		if(body.Name == "Enemy"){
+			GD.Print("Collision with Enemy");
+			//body.EmitSignal("on_player_hit");
+		}
+	}
+
+	private void _on_take_damage(float damage){
+		health -= damage;
+		GD.Print("Player took "+damage+" damage");
 	}
 }
+
+
+
